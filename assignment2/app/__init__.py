a8564377ab1fbb46679125bfeb2eb0a3e36b0f10
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-09-23 10:49:33 -0700

    added tracking id in logging

--- assignment2/app/__init__.py
+++ assignment2/app/__init__.py
@@ -2,9 +2,9 @@ import sys
 from flask import Flask, render_template
 from app.routes.FindAllItems import FIND_ALL_ITEMS
 from flask_cors import CORS, cross_origin
-import logging
-
-logging.basicConfig(level=logging.INFO)
+from app.logging.logger import *
+import uuid
+# logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 app.register_blueprint(FIND_ALL_ITEMS)
@@ -14,20 +14,21 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @cross_origin()
 @app.route("/")
 def hello_world():
-    logging.info("Rendering landing page")
+    LOGGER.info("Rendering landing page")
     return render_template('index.html')
 
 host="0.0.0.0"
 port="5050"
 debug=True
 if __name__ == "__main__":
+    # flask.g.trackingId=uuid.UUID(int=0)
     if(len(sys.argv) > 1):
         try:
             host=sys.argv[1]
             port=sys.argv[2]
             debug=bool(sys.argv[3])
         except:
-            logging.error("Invalid arguments")
+            LOGGER.error("Invalid arguments")
             sys.exit(1)
-    logging.info("Starting Flask Server on host %s, port %s, debug %s", host,port,debug)
+    LOGGER.info("Starting Flask Server on host "+host+", port "+port+", debug "+str(debug))
     app.run(host=host, debug=debug, port=port)
--- assignment2/app/routes/FindAllItems.py
+++ assignment2/app/routes/FindAllItems.py
@@ -1,8 +1,11 @@
+import sys
+sys.path.append("../app")
+
 from flask import Blueprint
 from flask import request
-import logging
+from app.logging.logger import *;
 
-logging.basicConfig(level=logging.INFO)
+
 
 FIND_ALL_ITEMS = Blueprint("findAllItems", __name__)
 
@@ -11,6 +14,7 @@ FIND_ALL_ITEMS = Blueprint("findAllItems", __name__)
 def findAllItems():
     print(request.args)
     trackingId = request.args.get("trackingId")
+    flask.g.trackingId = trackingId
     keyword = request.args.get("keyword")
     priceRangeFrom=request.args.get("from-number",None)
     priceRangeTo=request.args.get("to-number",None)
@@ -19,14 +23,14 @@ def findAllItems():
     shipping=request.args.get("shipping",None)
     sortBy=request.args.get("sort-by")
 
-    logging.info("Tracking Id: %s", str(trackingId))
-    logging.info("Keyword: %s", keyword)
-    logging.info("Price Range From: %s", priceRangeFrom)
-    logging.info("Price Range To: %s", priceRangeTo)
-    logging.info("Condition: %s", condition)
-    logging.info("Seller: %s", seller)
-    logging.info("Shipping: %s", shipping)
-    logging.info("Sort By: %s", sortBy)
+    LOGGER.info("Tracking Id: %s", str(trackingId))
+    LOGGER.info("Keyword: %s", keyword)
+    LOGGER.info("Price Range From: %s", priceRangeFrom)
+    LOGGER.info("Price Range To: %s", priceRangeTo)
+    LOGGER.info("Condition: %s", condition)
+    LOGGER.info("Seller: %s", seller)
+    LOGGER.info("Shipping: %s", shipping)
+    LOGGER.info("Sort By: %s", sortBy)
 
     return ("", 200)
 
