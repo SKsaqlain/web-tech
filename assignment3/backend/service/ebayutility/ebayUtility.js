a6de245be76e00b9be7132ed16b2156cdbf13275
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-10-13 12:35:14 -0700

    updated find all items xml request body-need to test

--- assignment3/backend/service/ebayutility/ebayUtility.js
+++ assignment3/backend/service/ebayutility/ebayUtility.js
@@ -1,73 +1,118 @@
-const logger=require('../../logging/logger')
-const xmlbuilder = require('xmlbuilder');
+const { log } = require("winston");
+const logger = require("../../logging/logger");
+const xmlbuilder = require("xmlbuilder");
 
-
-const ENUM = {"Best Match": "BestMatch",
-    "Price: highest first": "CurrentPriceHighest",
-    "Price + Shipping: highest first": "PricePlusShippingHighest",
-    "Price + Shipping: lowest first": "PricePlusShippingLowest",
-    "New": "1000",
-    "Used": "3000",
-    "Very Good": "4000",
-    "Good": "5000",
-    "Acceptable": "6000"
+const ENUM = {
+  "Best Match": "BestMatch",
+  "Price: highest first": "CurrentPriceHighest",
+  "Price + Shipping: highest first": "PricePlusShippingHighest",
+  "Price + Shipping: lowest first": "PricePlusShippingLowest",
+  New: "1000",
+  Used: "3000",
 };
+const CATEGORY_CODE = {
+  Art: "550",
+  Baby: "2984",
+  Books: "267",
+  "Clothing, Shoes & Accessories": "11450",
+  "Computers/Tablets & Networking": "58058",
+  "Health & Beauty": "26395",
+  Music: "11233",
+  "Video Games & Consoles": "1249",
+};
+
 // trackingId ,keyword, Category, Condition, Shipping Option, Distance, From
-function createXMLRequestPayload(trackingId, keyword, category, condition, shipping, distance, postalCode) {
-    try {
-        logger.info("Creating XML Request Payload",{trackingId});
-        const root = xmlbuilder.create('findItemsAdvancedRequest', { xmlns: 'http://www.ebay.com/marketplace/search/v1/services' });
+function createXMLRequestPayload(
+  trackingId,
+  keyword,
+  category,
+  condition,
+  shipping,
+  distance,
+  postalCode
+) {
+  try {
+    logger.info("Creating XML Request Payload", { trackingId });
+    const root = xmlbuilder.create("findItemsAdvancedRequest", {
+      xmlns: "http://www.ebay.com/marketplace/search/v1/services",
+    });
 
-        root.ele('keywords', keyword);
-        root.ele('buyerPostalCode',postalCode)
+    root.ele("keywords", keyword);
+    root.ele("buyerPostalCode", postalCode);
 
-        if (condition && condition.length >= 1) {
-            logger.info("Adding Condition tag",{trackingId})
-            const conditionTag = createFilterTag(root, 'Condition', ENUM[condition[0]]);
-            if (condition.length > 1) {
-                for (let i = 1; i < condition.length; i++) {
-                    conditionTag.ele('value', ENUM[condition[i]]);
-                }
-            }
-        }
-
-        if (shipping && shipping.length >= 1) {
-            if (shipping.includes('Free-Shipping')) {
-                createFilterTag(root, 'FreeShippingOnly', 'true');
-            if(shipping.includes('Local-Pickup'))
-                createFilterTag(root, 'LocalPickupOnly', 'true');
-            }
-            // for unspecified do not add tag
-        }
-
-        // need to add category id here.
-
-        if(distance && distance>0){
-            createFilterTag(root,'MaxDistance',distance);
-        }
-
-        createFilterTag(root,'HideDuplicateItems','true');
-
-
-        const paginationInput = root.ele('paginationInput');
-        paginationInput.ele('entriesPerPage', '60');
-
-        return root.end({ pretty: true });
-    } catch (error) {
-        logger.error('Error creating XML Request Payload',{trackingId});
-        throw new Error('Error creating XML Request Payload');
+    if (category && category.length >= 1) {
+      logger.info("Adding Category tag");
+      root.ele("categoryId", CATEGORY_CODE[category]);
     }
+    if (from && from.length > 0) {
+      logger.info("Adding From tag");
+      root.ele("buyerPostalCode", from);
+    }
+
+    if (distance) {
+      if (distance.length == 0) {
+        logger.info("Adding MaxDistance tag", { trackingId });
+        createFilterTag(root, "MaxDistance", "10");
+      } else {
+        createFilterTag(root, "MaxDistance", distance);
+      }
+    }
+
+    if (condition && condition.length >= 1) {
+      logger.info("Adding Condition tag", { trackingId });
+      const conditionTag = createFilterTag(
+        root,
+        "Condition",
+        ENUM[condition[0]]
+      );
+      // TODO: donot add unspecified code below.
+      if (condition.length > 1) {
+        for (let i = 1; i < condition.length; i++) {
+          conditionTag.ele("value", ENUM[condition[i]]);
+        }
+      }
+    }
+
+    if (shipping && shipping.length >= 1) {
+        logger.info("Adding Shipping tag", { trackingId });
+      if (shipping.includes("Free-Shipping")) {
+        logger.info("Adding FreeShippingOnly tag", { trackingId });
+        createFilterTag(root, "FreeShippingOnly", "true");
+      }
+
+      if (shipping.includes("Local-Pickup")) {
+        logger.info("Adding LocalPickupOnly tag", { trackingId });
+        createFilterTag(root, "LocalPickupOnly", "true");
+      }
+
+      // for unspecified do not add tag
+    }
+
+    if (distance && distance > 0) {
+        logger.info("Adding MaxDistance tag", { trackingId });
+      createFilterTag(root, "MaxDistance", distance);
+    }
+
+    createFilterTag(root, "HideDuplicateItems", "true");
+
+    const paginationInput = root.ele("paginationInput");
+    paginationInput.ele("entriesPerPage", "60");
+
+    return root.end({ pretty: true });
+  } catch (error) {
+    logger.error("Error creating XML Request Payload", { trackingId });
+    throw new Error("Error creating XML Request Payload");
+  }
 }
 
 function createFilterTag(root, name, value) {
-    const filterTag = root.ele('itemFilter');
-    filterTag.ele('name', name);
-    filterTag.ele('value', value);
-    return filterTag;
+  const filterTag = root.ele("itemFilter");
+  filterTag.ele("name", name);
+  filterTag.ele("value", value);
+  return filterTag;
 }
 
-
-module.exports={
-    createXMLRequestPayload,
-    createFilterTag,
+module.exports = {
+  createXMLRequestPayload,
+  createFilterTag,
 };
