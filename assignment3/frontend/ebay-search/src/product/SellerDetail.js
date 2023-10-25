ec9d50f35035289e542e326c5ce94d37aa904c9c
Saqlain Shaik <sms@Saqlains-MacBook-Pro.local>
2023-10-25 11:55:04 -0700

    added all material icons to seller details tab

--- assignment3/frontend/ebay-search/src/product/SellerDetail.js
+++ assignment3/frontend/ebay-search/src/product/SellerDetail.js
@@ -1,103 +1,145 @@
-import { render } from '@testing-library/react'
-import './SellerDetail.css'
+import { render } from "@testing-library/react";
+import "./SellerDetail.css";
 
-
-import { CircularProgressbar,buildStyles  } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
+import StarsIcon from "@mui/icons-material/Stars";
 
 const SellerDetail = (props) => {
-    const renderSellerName = () => {
-        if(props.item.sellerDetails.storeName){
-            return (
-                <div class="row align-items-center justify-content-center">
-                    <div class="col text-center sellerName" style={{color:'white'}}>{props.item.sellerDetails.storeName}</div>
-                </div>
-            )
-        }
+  const colorRGBValues = {
+    None: "rgb(0, 0, 0)", // Black
+    Yellow: "rgb(255, 255, 0)",
+    Blue: "rgb(0, 0, 255)",
+    Turquoise: "rgb(64, 224, 208)",
+    Purple: "rgb(128, 0, 128)",
+    Red: "rgb(255, 0, 0)",
+    Green: "rgb(0, 128, 0)",
+    YellowShooting: "rgb(255, 215, 0)",
+    TurquoiseShooting: "rgb(0, 191, 255)",
+    PurpleShooting: "rgb(160, 32, 240)",
+    RedShooting: "rgb(220, 20, 60)",
+    GreenShooting: "rgb(0, 128, 0)",
+    SilverShooting: "rgb(192, 192, 192)", // Silver
+  };
+
+  // Example usage:
+  const colorName = "Yellow"; // Replace with the color name you want
+  const rgbValue = colorRGBValues[colorName];
+  console.log(`Color Name: ${colorName}, RGB Value: ${rgbValue}`);
+
+  const renderSellerName = () => {
+    if (props.item.sellerDetails.storeName) {
+      return (
+        <div class="row align-items-center justify-content-center">
+          <div class="col text-center sellerName" style={{ color: "white" }}>
+            {props.item.sellerDetails.storeName}
+          </div>
+        </div>
+      );
     }
-    const renderFeedbackScore = () => {
-        if (props.item.sellerDetails.feedBackScore) {
-          return (
-            <div class="row shippingDetailsRow">
-              <div class="col shippingFieldName">Feedback Score</div>
-              <div class="col">{props.item.sellerDetails.feedBackScore}</div>
-            </div>
-          );
-        }
-      }
+  };
+  const renderFeedbackScore = () => {
+    if (props.item.sellerDetails.feedBackScore) {
+      return (
+        <div class="row shippingDetailsRow">
+          <div class="col shippingFieldName">Feedback Score</div>
+          <div class="col">{props.item.sellerDetails.feedBackScore}</div>
+        </div>
+      );
+    }
+  };
 
-      const renderPopularity = () => {
-        if (props.item.sellerDetails.popularity) {
-            const popularity=parseFloat(props.item.sellerDetails.popularity);
-          return (
-            <div class="row shippingDetailsRow">
-              <div class="col shippingFieldName">Popularity</div>
-              {/* <div class="col">{props.item.sellerDetails.popularity}</div> */}
-              <div class="col"><div style={{width:'3rem', height:'3rem'}}>
-                  <CircularProgressbar value={popularity} text={`${popularity}`} styles={buildStyles({pathColor:'green',textColor:'white'})} />
-              </div></div>
+  const renderPopularity = () => {
+    if (props.item.sellerDetails.popularity) {
+      const popularity = parseFloat(props.item.sellerDetails.popularity);
+      return (
+        <div class="row shippingDetailsRow">
+          <div class="col shippingFieldName">Popularity</div>
+          {/* <div class="col">{props.item.sellerDetails.popularity}</div> */}
+          <div class="col">
+            <div style={{ width: "3rem", height: "3rem" }}>
+              <CircularProgressbar
+                value={popularity}
+                text={`${popularity}`}
+                styles={buildStyles({ pathColor: "green", textColor: "white" })}
+              />
             </div>
-          );
-        }
-      }
-      const renderFeedbackRatingStar = () => {
-        if (props.item.sellerDetails.feedbackRatingStar) {
-          return (
-            <div class="row shippingDetailsRow">
-              <div class="col shippingFieldName">Feedback Rating Star</div>
-              <div class="col">{props.item.sellerDetails.feedbackRatingStar}</div>
-            </div>
-          );
-        }
-      }
-      const renderTopRated = () => {
-        if (props.item.sellerDetails.topRated) {
-          return (
-            <div class="row shippingDetailsRow">
-              <div class="col shippingFieldName">Top Rated</div>
-              <div class="col">{props.item.sellerDetails.topRated === "true" ? (
-              <CheckIcon style={{ color: "green" }} />
+          </div>
+        </div>
+      );
+    }
+  };
+  
+  const renderFeedbackRatingStar = () => {
+    if (props.item.sellerDetails.feedbackRatingStar) {
+      return (
+        <div class="row shippingDetailsRow">
+          <div class="col shippingFieldName">Feedback Rating Star</div>
+          <div class="col">
+            <StarsIcon
+              style={{
+                color: `${
+                    props.item.sellerDetails.feedbackRatingStar!='None'?colorRGBValues[`${props.item.sellerDetails.feedbackRatingStar}`]:colorRGBValues['None']
+                }`,
+                width: "3rem", height: "3rem"
+              }}
+            />
+          </div>
+        </div>
+      );
+    }
+  };
+  const renderTopRated = () => {
+    if (props.item.sellerDetails.topRated) {
+      return (
+        <div class="row shippingDetailsRow">
+          <div class="col shippingFieldName">Top Rated</div>
+          <div class="col">
+            {props.item.sellerDetails.topRated === "true" ? (
+              <CheckIcon style={{ color: "green" ,width: "3rem", height: "3rem" }} />
             ) : (
-              <CloseIcon style={{ color: "red" }} />
-            )}</div>
-            </div>
-          );
-        }
-      }
-      const renderStoreName = () => {
-        if (props.item.sellerDetails.storeName) {
-          return (
-            <div class="row shippingDetailsRow">
-              <div class="col shippingFieldName">Store Name</div>
-              <div class="col">{props.item.sellerDetails.storeName}</div>
-            </div>
-          );
-        }
-      }
-      const renderBuyProductAt = () => {
-        if (props.item.sellerDetails.buyProductAt) {
-          return (
-            <div class="row shippingDetailsRow">
-              <div class="col shippingFieldName">Buy Product At</div>
-              <div class="col"><a href={props.item.sellerDetails.buyProductAt}>Store</a></div>
-            </div>
-          );
-        }
-      }
+              <CloseIcon style={{ color: "red" ,width: "3rem", height: "3rem" }} />
+            )}
+          </div>
+        </div>
+      );
+    }
+  };
+  const renderStoreName = () => {
+    if (props.item.sellerDetails.storeName) {
+      return (
+        <div class="row shippingDetailsRow">
+          <div class="col shippingFieldName">Store Name</div>
+          <div class="col">{props.item.sellerDetails.storeName}</div>
+        </div>
+      );
+    }
+  };
+  const renderBuyProductAt = () => {
+    if (props.item.sellerDetails.buyProductAt) {
+      return (
+        <div class="row shippingDetailsRow">
+          <div class="col shippingFieldName">Buy Product At</div>
+          <div class="col">
+            <a href={props.item.sellerDetails.buyProductAt}>Store</a>
+          </div>
+        </div>
+      );
+    }
+  };
 
-    if (
-        props.productState.isSellerPage &&
-        props.item &&
-        props.item.sellerDetails
-      ) {
-        console.log("rendering shipping details");
-        console.dir(props.item);
-        return(
-            <div
+  if (
+    props.productState.isSellerPage &&
+    props.item &&
+    props.item.sellerDetails
+  ) {
+    console.log("rendering shipping details");
+    console.dir(props.item);
+    return (
+      <div
         key={props.item.itemId + "_itemSellerDetails"}
         class="container item-seller-details-container"
       >
@@ -109,9 +151,8 @@ const SellerDetail = (props) => {
         {renderStoreName()}
         {renderBuyProductAt()}
       </div>
-        );
-      }
-
-}
+    );
+  }
+};
 
 export default SellerDetail;
