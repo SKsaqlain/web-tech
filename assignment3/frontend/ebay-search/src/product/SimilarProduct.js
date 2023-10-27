3a0c05e739189a1a91b10983a3279e110ada7ec3
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-10-27 08:13:18 -0700

    added sortby and order by btns

--- assignment3/frontend/ebay-search/src/product/SimilarProduct.js
+++ assignment3/frontend/ebay-search/src/product/SimilarProduct.js
@@ -8,29 +8,50 @@ import SimilarProductItem from "./SimilarProductItem";
 const SimilarProduct = (props) => {
   const [similarProductState, setSimilarProductState] = useState({
     similarProducts: [],
-    sortBy: "Default",
+    sortBy: "default",
     sortOrder: "ascending",
   });
 
+  const handleSortChange = (event) => {
+    console.log("sort by changed to " + event.target.value);
+    setSimilarProductState((prevState) => {
+      return {
+        ...prevState,
+        sortBy: event.target.value,
+      };
+    });
+  }
+
+
   const renderSortByDropDown = () => {
     return (
-      <select class="form-select" aria-label="sort-by">
-        <option selected>Default</option>
-        <option value="1">Product Name</option>
-        <option value="2">Days Left</option>
-        <option value="3">Price</option>
-        <option value="3">Shipping Cost</option>
+      <select class="form-select" aria-label="sort-by" onChange={handleSortChange}>
+        <option selected value="default">Default</option>
+        <option value="productName">Product Name</option>
+        <option value="daysLeft">Days Left</option>
+        <option value="price">Price</option>
+        <option value="shippingCost">Shipping Cost</option>
       </select>
     );
   };
 
+  const handleOrderChange=(event)=>{
+    console.log("order by changed to " + event.target.value);
+    setSimilarProductState((prevState) => {
+      return {
+        ...prevState,
+        sortOrder: event.target.value,
+      };
+    });
+  }
+
   const renderOrderByDropDown = () => {
     return (
-      <select class="form-select" aria-label="sort-order">
-        <option value="Ascending" selected>
+      <select class="form-select" aria-label="sort-order" onChange={handleOrderChange}>
+        <option value="ascending" selected>
           Ascending
         </option>
-        <option value="Descending">Descending</option>
+        <option value="descending">Descending</option>
       </select>
     );
   };
@@ -43,6 +64,17 @@ const SimilarProduct = (props) => {
     }
   };
 
+  const parseFieldsType=(details)=>{
+    return details.map((item)=>{
+      return{
+        ...item,
+        price: parseFloat(item.price),
+        shippingCost: parseFloat(item.shippingCost),
+        daysLeft: parseInt(item.daysLeft),
+      }
+    });
+  }
+
   if (
     props.productState.isSimilarProducts &&
     similarProductState.similarProducts.length == 0
@@ -53,10 +85,11 @@ const SimilarProduct = (props) => {
         "Received similar product detail for item " + props.item.itemId
       );
       console.dir(details);
+      const parsedDetails=parseFieldsType(details)
       setSimilarProductState((prevState) => {
         return {
           ...prevState,
-          similarProducts: details,
+          similarProducts: parsedDetails,
         };
       });
     });
