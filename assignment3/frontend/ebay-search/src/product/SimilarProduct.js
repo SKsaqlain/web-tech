75fcd327cd0f5faf9a1842f32e314060ccafdb69
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-10-26 23:35:00 -0700

    displayed similar products

--- assignment3/frontend/ebay-search/src/product/SimilarProduct.js
+++ assignment3/frontend/ebay-search/src/product/SimilarProduct.js
@@ -2,30 +2,49 @@ import { fetchSimilarItems } from "../services/EbaySearchApi";
 
 import React, { useEffect, useState } from "react";
 
+import "./SimilarProduct.css";
+import SimilarProductItem from "./SimilarProductItem";
+
 const SimilarProduct = (props) => {
+  const [similarProductState, setSimilarProductState] = useState({});
 
-    const[similarProductState,setSimilarProductState]=useState({});
-
-        
-      
-    if(props.productState.isSimilarProducts && !similarProductState.similarProducts){
-      console.log("fetching similar products");
-      fetchSimilarItems(props.item.itemId).then((details) => {
-        console.log("Received similar product detail for item " + props.item.itemId);
-        console.dir(details);
-        setSimilarProductState((prevState)=>{
-          return{
-            ...prevState,
-            similarProducts: details,
-          }
-        });
+  const renderSimilarItems = () => {
+    if (similarProductState.similarProducts) {
+      return similarProductState.similarProducts.map((item) => {
+        return <SimilarProductItem item={item} />;
       });
     }
-    if(props.productState.isSimilarProducts && similarProductState.similarProducts?.length>0){
-      console.log("rendering similar products");
-     return <div>Similar Products</div>
-    }
+  };
 
+  if (
+    props.productState.isSimilarProducts &&
+    !similarProductState.similarProducts
+  ) {
+    console.log("fetching similar products");
+    fetchSimilarItems(props.item.itemId).then((details) => {
+      console.log(
+        "Received similar product detail for item " + props.item.itemId
+      );
+      console.dir(details);
+      setSimilarProductState((prevState) => {
+        return {
+          ...prevState,
+          similarProducts: details,
+        };
+      });
+    });
+  }
+  if (
+    props.productState.isSimilarProducts &&
+    similarProductState.similarProducts?.length > 0
+  ) {
+    console.log("rendering similar products");
+    return (
+      <div>
+        {renderSimilarItems()}
+      </div>
+    );
+  }
 };
 
 export default SimilarProduct;
---
+++ assignment3/frontend/ebay-search/src/product/SimilarProductItem.js
@@ -0,0 +1,17 @@
+const SimilarProductItem = (props) => {
+    // console.log("rendering similar product item");
+  if (props.item) {
+    return (
+      <div class="row similarProductItemDetailsRow" key={props.item.itemId+"_similarItem"}>
+        <div class="col-md-4"> <img src={props.item.imageURL} alt="Image Not Found" /> </div>
+        <div class="col-md-8">
+          <div class="pb-3">{props.item.productName}</div>
+          <div class="pb-3">Price:${props.item.price}</div>
+          <div class="pb-3">Shipping Cost: ${props.item.shippingCost}</div>
+          <div class="pb-3">Days Left: ${props.item.daysLeft}</div>
+        </div>
+      </div>
+    );
+  }
+};
+export default SimilarProductItem;
