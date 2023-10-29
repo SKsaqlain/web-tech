ec4ef95bb0198e0549f53902e4fe0b7c5341b1fc
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-10-28 20:12:20 -0700

    added similar-product-color

--- assignment3/frontend/ebay-search/src/product/SimilarProductItem.js
+++ assignment3/frontend/ebay-search/src/product/SimilarProductItem.js
@@ -6,10 +6,10 @@ const SimilarProductItem = (props) => {
       <div class="row similarProductItemDetailsRow" key={props.item.itemId+"_similarItem"}>
         <div class="col-md-4"> <img src={props.item.imageURL} alt="Image Not Found" /> </div>
         <div class="col-md-8">
-          <div class="pb-3">{props.item.productName}</div>
-          <div class="pb-3">Price:${props.item.price}</div>
-          <div class="pb-3">Shipping Cost: ${props.item.shippingCost}</div>
-          <div class="pb-3">Days Left: ${props.item.daysLeft}</div>
+          <div class="pb-3 productName"><a href={props.item.viewItemURL} target='_blank' className="similarProductItemUrl">{props.item.productName}</a></div>
+          <div class="pb-3 price">Price:${props.item.price}</div>
+          <div class="pb-3 shippingCost">Shipping Cost: ${props.item.shippingCost}</div>
+          <div class="pb-3 daysLeft">Days Left: {props.item.daysLeft}</div>
         </div>
       </div>
     );
