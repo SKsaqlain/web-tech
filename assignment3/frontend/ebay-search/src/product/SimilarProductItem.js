9922600df011febd2aa86175a283a897e69328a5
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-10-30 10:59:10 -0700

    fixed similar product style

--- assignment3/frontend/ebay-search/src/product/SimilarProductItem.js
+++ assignment3/frontend/ebay-search/src/product/SimilarProductItem.js
@@ -1,15 +1,22 @@
-import './SimilarProductItem.css';
+import "./SimilarProductItem.css";
 const SimilarProductItem = (props) => {
-    // console.log("rendering similar product item");
+  // console.log("rendering similar product item");
   if (props.item) {
     return (
-      <div class="card row similarProductItemDetailsRow" key={props.item.itemId+"_similarItem"}>
-        <div class="col-md-4"> <img src={props.item.imageURL} alt="Image Not Found" /> </div>
-        <div class="col-md-8">
-          <div class="pb-3 productName"><a href={props.item.viewItemURL} target='_blank' className="similarProductItemUrl">{props.item.productName}</a></div>
-          <div class="pb-3 price">Price:${props.item.price}</div>
-          <div class="pb-3 shippingCost">Shipping Cost: ${props.item.shippingCost}</div>
-          <div class="pb-3 daysLeft">Days Left: {props.item.daysLeft}</div>
+      <div class='card row similarProductItemDetailsRow' key={props.item.itemId + "_similarItem"}>
+        <div class='col-md-4'>
+          {" "}
+          <img src={props.item.imageURL} alt='Image Not Found' style={{ width: "8rem", height: "8rem" }} />{" "}
+        </div>
+        <div class='col-md-8 align-self-start' style={{ fontSize: "small" }}>
+          <div class='pb-1 productName'>
+            <a href={props.item.viewItemURL} target='_blank' className='similarProductItemUrl'>
+              {props.item.productName}
+            </a>
+          </div>
+          <div class='pb-1 price'>Price:${props.item.price}</div>
+          <div class='pb-1 shippingCost'>Shipping Cost: ${props.item.shippingCost}</div>
+          <div class='pb-1 daysLeft'>Days Left: {props.item.daysLeft}</div>
         </div>
       </div>
     );
