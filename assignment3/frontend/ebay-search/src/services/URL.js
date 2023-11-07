26796f5568d53d8e1297f8c230fec7e9b7ffc8c3
Saqlain Shaik <sms@Saqlains-MacBook-Pro.local>
2023-11-07 15:22:08 -0800

    updated no-records component style

--- assignment3/frontend/ebay-search/src/services/URL.js
+++ assignment3/frontend/ebay-search/src/services/URL.js
@@ -1,4 +1,3 @@
-const URL="http://localhost:8080";
-// const URL="https://sms-wt-assgn3.uw.r.appspot.com";
-
+// const URL="http://localhost:8080";
+const URL="https://sms-wt-assgn3.uw.r.appspot.com";
 export default URL;
