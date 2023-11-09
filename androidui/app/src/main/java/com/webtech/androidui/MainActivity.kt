b6fe0a995e7603513c5f026d050b6d5f4603853f
Saqlain Shaik <sms@Saqlains-MacBook-Pro.local>
2023-11-09 15:19:14 -0800

    added categories

--- androidui/app/src/main/java/com/webtech/androidui/MainActivity.kt
+++ androidui/app/src/main/java/com/webtech/androidui/MainActivity.kt
@@ -2,10 +2,17 @@ package com.webtech.androidui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.webtech.androidui.productsearch.ProductSearch
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.form_search_fragment)
+        val productSearch: ProductSearch = ProductSearch()
+        productSearch.spinnerAdapter(findViewById(R.id.category))
     }
 }
---
+++ androidui/app/src/main/java/com/webtech/androidui/productsearch/ProductSearch.kt
@@ -0,0 +1,20 @@
+package com.webtech.androidui.productsearch
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import com.webtech.androidui.R
+
+class ProductSearch(){
+
+    fun spinnerAdapter( spinner: Spinner) {
+        val adapter = ArrayAdapter.createFromResource(
+            spinner.context,
+            R.array.categories,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+}
