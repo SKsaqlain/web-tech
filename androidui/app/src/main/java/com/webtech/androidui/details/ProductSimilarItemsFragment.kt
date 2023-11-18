a7cda09c0100f9d676dde915eb5725fba1cb7bfd
Saqlain Shaik <sms@Saqlains-MBP.lan>
2023-11-18 15:53:01 -0800

    added sort feature

--- androidui/app/src/main/java/com/webtech/androidui/details/ProductSimilarItemsFragment.kt
+++ androidui/app/src/main/java/com/webtech/androidui/details/ProductSimilarItemsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.lifecycle.ViewModelProvider
@@ -46,28 +47,75 @@ class ProductSimilarItemsFragment : Fragment() {
         uiState.setSimilarProductsResponse(similarProductsResponse)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    fun sortData(spinnerSortMethod: Spinner, spinnerSortOrder: Spinner) {
+        val uiState = ViewModelProvider(requireActivity()).get(UIState::class.java)
+        val sortMethod = spinnerSortMethod.selectedItem.toString()
+        val sortOrder = spinnerSortOrder.selectedItem.toString()
+
+        val sortedList = uiState.similarProductsResponse.value?.sortedWith(Comparator { o1, o2 ->
+            when (sortMethod) {
+                "Price" -> compareValuesBy(o1, o2, { it.price.toDouble() })
+                "Name" -> compareValuesBy(o1, o2, { it.productName })
+                "Days" -> compareValuesBy(o1, o2, { it.daysLeft.toInt() })
+                else -> 0
+            }
+        })
+
+        if (sortOrder == "Descending") {
+            sortedList?.reversed()
+        }
+
+        uiState.similarProductsResponse.postValue(sortedList)
+    }
+
+
+    fun createSortDropDown(view: View) {
+        logger.info("Creating Sort Drop Down for similar items")
         val spinnerSortMethod: Spinner = view.findViewById(R.id.spinnerSortMethod)
         val spinnerSortOrder: Spinner = view.findViewById(R.id.spinnerSortOrder)
 
-// Example data for Spinners
-        val sortMethods = arrayOf("Default", "Name", "Price","Days")
+
+        val sortMethods = arrayOf("Default", "Name", "Price", "Days")
         val sortOrders = arrayOf("Ascending", "Descending")
 
-// Adapters for Spinners
-        val adapterSortMethods = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sortMethods)
+
+        val adapterSortMethods =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sortMethods)
         adapterSortMethods.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerSortMethod.adapter = adapterSortMethods
 
-        val adapterSortOrders = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sortOrders)
+        val adapterSortOrders =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sortOrders)
         adapterSortOrders.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerSortOrder.adapter = adapterSortOrders
 
+        spinnerSortMethod.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                sortData(spinnerSortMethod, spinnerSortOrder)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        spinnerSortOrder.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                sortData(spinnerSortMethod, spinnerSortOrder)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createSortDropDown(view)
         val uiState = ViewModelProvider(requireActivity()).get(UIState::class.java)
         val itemId = uiState.productDetails.value?.itemId
         if (itemId != null) {
-            ebayService.findSimilarItems(view, itemId,::updateSimilarItems)
+            ebayService.findSimilarItems(view, itemId, ::updateSimilarItems)
         }
         uiState.similarProductsResponse.observe(viewLifecycleOwner) {
             if (it == null)
@@ -75,7 +123,7 @@ class ProductSimilarItemsFragment : Fragment() {
             logger.info("Similar Products Response: $it")
             val similarItemsAdaptor = SimilarItemsAdaptor(it)
             val recyclerView = view.findViewById<RecyclerView>(R.id.similarProductView)
-            recyclerView.layoutManager=LinearLayoutManager(context)
+            recyclerView.layoutManager = LinearLayoutManager(context)
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = similarItemsAdaptor
         }
